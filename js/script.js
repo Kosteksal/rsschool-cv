b82a8da86c91dcb9af4cf24a112df217a40a17d5
Kosteksal <kosteks@outlook.com>
2021-12-30 11:45:07 +0300

    feat: add about

--- js/script.js
+++ js/script.js
@@ -3,7 +3,6 @@ $(document).ready(function() {
         $(this).toggleClass('open-menu');
         $('#nav').toggleClass('nav--open');
         $('.container__header').toggleClass('container__header--open');
-        $('.header__tel').toggleClass('header__tel--open');
         $('.body').toggleClass('body--open');
     });
 });
